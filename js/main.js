bec3689ba5064911a8a4ee2eb035f1473da2931f
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-05-08 19:32:34 -0300

    feito comando para salvar itens dentro da lista

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-let listaDeCompras = ['Arroz', 'Biscoito', 'Suco'];
+let listaDeItens = [];
 
-let listaDeItens = []
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
 
-listaDeItens.mostrarItens()
+//Criando interatividade com o botão da linha 24 do HTML, type submit
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+    salvarItem()
+});
+
+function salvarItem() {
+    const comprasItem = itensInput.value;
+    //toUpperCase funcionalidade para deixar resultado todo maiusculo,
+    // não deixando duplicidade de palavras acontecer mesmo colocando minusculo.
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase())
+
+    if (checarDuplicado) {
+        alert("Item já cadastrado!")
+    } else {
+        //push para inserir e clonar objetos e colocar na lista
+        listaDeItens.push({
+            valor: comprasItem
+        })
+    }
+
+
+    console.log(listaDeItens);
+}
