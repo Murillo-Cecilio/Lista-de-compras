4a0e82b81c49835c4d5243b28beab3aab0a614ae
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-05-10 10:24:27 -0300

    configuração de editar itens

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [];
+let itemAEditar
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
@@ -57,6 +58,7 @@ function mostrarItem() {
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+            <button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -80,9 +82,22 @@ function mostrarItem() {
     deletarObjetos.forEach(i => {
         i.addEventListener('click', (evento) => {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
-            listaDeItens[valorDoElemento].checar = evento.target.checked
             listaDeItens.splice(valorDoElemento, 1);
             mostrarItem();
         });
     });
+
+    const editarItens = document.querySelectorAll(".editar");
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value');
+            mostrarItem();
+        });
+    });
+}
+
+function salvarEdicao() {
+    const itemAEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`);
+    console.log(itemAEditado.value);
 }
